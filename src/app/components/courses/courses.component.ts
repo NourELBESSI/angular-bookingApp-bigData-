28b62aa1a38c847f659c9947520e2e7f2f17dbe1
NourELBESSI <nour.elbessi@ensiie.fr>
2023-01-20 09:38:15 +0100

    dockerise front

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -12,6 +12,11 @@ import { CourseService } from 'src/app/services/course.service';
 export class CoursesComponent implements OnInit {
 
   public courses: Tutorial[] = [];
+  public courses2: Tutorial[] = [{id: "1", title: "Angular", tutorialType: "Beginner", content: "Angular"},
+    {id: "2", title: "C#", tutorialType: "Beginner", content: "C#"},
+    {id: "3", title: "Java", tutorialType: "Beginner", content: "Java"},
+    {id: "4", title: "Python", tutorialType: "Beginner", content: "Python"},
+    {id: "5", title: "C++", tutorialType: "Beginner", content: "C++"}]
   public loaded:boolean = false;
 
 
